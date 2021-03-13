fb7fec8428c68add1c7c3c288c668cb174ff1177
ducson20 <leson2620@gmail.com>
2021-03-13 10:08:43 +0700

    Son commit

--- LibraryManagement/Controllers/HomeController.cs
+++ LibraryManagement/Controllers/HomeController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using LibraryManagement.Models;
 namespace LibraryManagement.Controllers {
     public class HomeController : Controller {
+        private LibraryEntities _db = new LibraryEntities();
         public ActionResult Index() {
             return View();
         }
