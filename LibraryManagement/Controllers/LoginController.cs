a668f6bb2f0a0a0d1b55fe5cac1f8fcdc6da3077
Nguyen Xuan Nghiep <nguyenxuannghiep1710@gmail.com>
2021-03-14 10:23:59 +0700

    fix something in login

--- LibraryManagement/Controllers/LoginController.cs
+++ LibraryManagement/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using LibraryManagement.Models;
 namespace LibraryManagement.Controllers
 {
@@ -32,6 +33,11 @@ namespace LibraryManagement.Controllers
                 }
             }
             return View();
+        }
+
+        public ActionResult LogOut() {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
